67e1956e18b05243be2ef054535b1461795525e1
szimin <szimin18@gmail.com>
2014-04-16 14:55:29 +0200

    updated readme

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
-(defproject hypergraphdb-tutorial "0.1.0-SNAPSHOT"
+(defproject hypergraphdb_tutorial "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.hypergraphdb/hgdb "1.2"]
+                 [org.hypergraphdb/hgbdbje "1.2"]
+                 [com.sleepycat/je "5.0.73"]
+                 [org.clojure/tools.logging "0.2.6"]]
+  :main hypergraphdb_tutorial.core)
--- src/hypergraphdb_tutorial/core.clj
+++ src/hypergraphdb_tutorial/core.clj
@@ -1,6 +1,22 @@
-(ns hypergraphdb-tutorial.core)
+(ns hypergraphdb_tutorial.core
+  (:import [org.hypergraphdb HGEnvironment])
+  (:gen-class :main true)
+  (:use [clojure.tools.logging :only (info)]))
 
-(defn foo
+
+(def database (atom nil))
+
+
+(defn create-database
+  "
+  Creates a database or opens existing one from the folder specified by argument
+  "
+  [path]
+  (let [dbinstance (HGEnvironment/get path)]
+    (reset! database dbinstance)))
+
+
+(defn -main
   "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  []
+  (create-database "hgdbtest"))
