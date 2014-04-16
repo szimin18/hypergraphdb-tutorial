85695fa3890677e7cccae01085013eb844a27064
szimin <szimin18@gmail.com>
2014-04-16 13:28:09 +0200

    init commit

---
+++ test/hypergraphdb_tutorial/core_test.clj
@@ -0,0 +1,7 @@
+(ns hypergraphdb-tutorial.core-test
+  (:require [clojure.test :refer :all]
+            [hypergraphdb-tutorial.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
