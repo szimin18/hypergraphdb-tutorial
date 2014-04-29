1e2cfb3d820ba795ac538c035a51fc1367d8a3bf
szimin <szimin18@gmail.com>
2014-04-29 15:51:01 +0200

    added all of example functionality

--- src/hypergraphdb_tutorial/core.clj
+++ src/hypergraphdb_tutorial/core.clj
@@ -1,11 +1,13 @@
 (ns hypergraphdb_tutorial.core
-  (:import [org.hypergraphdb HGEnvironment HGQuery])
+  (:import (org.hypergraphdb HGEnvironment HGHandle HGQuery$hg HGPlainLink)
+           (java.util LinkedList))
   (:gen-class :main true)
   (:use [clojure.tools.logging :only (info)]))
 
 
 (def database (atom nil))
-(def handle (atom nil))
+(def handler (atom nil))
+(def data-list (atom nil))
 
 
 (defn create-database
@@ -17,22 +19,63 @@
     (reset! database dbinstance)))
 
 
-(defn add-example
+(defn add-single-data
   [example]
-  (reset! handle (.add @database example)))
+  (reset! handler (.add @database example)))
 
 
-(defn get-data
+(defn get-single-data
   []
-  (.get @database @handle))
+  (.get @database @handler))
 
 
-(defn remove-example
+(defn add-linked-data
   []
-  (.remove @database @handle))
+  (let [item-handlers (map #(.add @database %) (list 1 2 3 4 5))]
+    (reduce #(do (.add @database (new HGPlainLink (into-array HGHandle [%1 %2]))) %2) item-handlers)))
 
 
-(defn close-graph
+(defn get-linked-data
+  []
+  (HGQuery$hg/getAll @database (HGQuery$hg/type HGPlainLink)))
+
+
+(defn print-linked-data
+  [linked-data]
+  (doall (map #(println (str (.get @database (.getTargetAt % 0)) " " (.get @database (.getTargetAt % 1)))) linked-data)))
+
+
+(defn create-list
+  []
+  (do
+    (reset! data-list (new LinkedList))
+    (.add @data-list (new Integer 1))
+    (.add @data-list (new Integer 2))
+    (.add @data-list (new Integer 3))
+    ))
+
+
+(defn add-list-entry
+  []
+  (reset! handler (.add @database @data-list)))
+
+
+(defn print-list
+  []
+  (println (clojure.string/join " " (.get @database @handler))))
+
+
+(defn modify-list
+  []
+  (.add @data-list (new Integer 4)))
+
+
+(defn update-list-entry
+  []
+  (.update @database @data-list))
+
+
+(defn close-database
   []
   (.close @database))
 
@@ -41,12 +84,50 @@
   []
   (do
     (create-database "hgdbtest")
-    (add-example {:hello "world"})
-    (println (get-data))
-    (remove-example)
-    (close-graph)
+    (add-single-data {:hello "world"})
+    (println (get-single-data))
+    (close-database)
     ))
 
+
+(defn example-two
+  []
+  (do
+    (create-database "hgdbtest")
+    (add-linked-data)
+    (print-linked-data (get-linked-data))
+    (close-database)
+    ))
+
+
+(defn example-three
+  []
+  (do
+    (create-database "hgdbtest")
+    (create-list)
+    (add-list-entry)
+    (print-list)
+    (modify-list)
+    (update-list-entry)
+    (print-list)
+    (close-database)
+    ))
+
+
+(defn example-four
+  []
+  (do
+    (create-database "hgdbtest")
+    (.beginTransaction (.getTransactionManager @database))
+    (try
+      (.add @database (new Integer (/ 1 0)))
+      (.commit (.getTransactionManager @database))
+      (println "Transaction commited")
+      (catch Exception e (do
+                           (.abort (.getTransactionManager @database))
+                           (println "Transaction aborted"))))
+    (close-database)
+    ))
 
 (defn -main
   []
